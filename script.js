9d3beeb7ce2bf94eee22938b0a1832088183956c
unhollyX <117582945+UnhollyX@users.noreply.github.com>
2024-04-04 15:20:39 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('toggle1').addEventListener('change', function() {
+    // Toggle switch 1
+  });
+  
+  document.getElementById('toggle2').addEventListener('change', function() {
+    // Toggle switch 2
+  });
+  
+  document.getElementById('toggle3').addEventListener('change', function() {
+    // Toggle switch 3
+  });
